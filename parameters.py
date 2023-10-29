ad28fa5cef324b5c32100d3ae2d06bbd1f6271d3
Albert Arnó <alarno@onmedic.com>
2023-10-29 10:55:24 +0100

    sbd

--- parameters.py
+++ parameters.py
@@ -1,6 +1,6 @@
-file_name = 'connections.csv'
-linkedin_username = 'XXXXX@gmail.com'
-linkedin_password = 'XXXXXX'
-keywords = 'DevOps,Linux,Python,AWS'
-ignore_list = 'Person1,Person2'
+file_name = "connections.csv"
+linkedin_username = "freed4catalonia@gmail.com"
+linkedin_password = "canadaconan17"
+keywords = "DevOps,Linux,Python,AWS"
+ignore_list = "Person1,Person2"
 till_page = 10
